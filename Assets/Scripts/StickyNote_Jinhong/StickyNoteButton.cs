49cc7ef3d3c48299ea65ddd07a1f299810c3572b
Sung Hoon Bae <70627437+MondaySHB@users.noreply.github.com>
2022-01-29 00:24:36 +0900

    stickynote network 확인

--- Assets/Scripts/StickyNote_Jinhong/StickyNoteButton.cs
+++ Assets/Scripts/StickyNote_Jinhong/StickyNoteButton.cs
@@ -1,10 +1,15 @@
+using Photon.Pun;
+using Photon.Realtime;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class StickyNoteButton : MonoBehaviour
 {
     public GameObject stickyNotePrefab;
+    private static PhotonTransformView transformView;
+    private static PhotonView view;
 
     public void CreateStickyNote()
     {
